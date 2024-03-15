d7752b2974eaf13f7d67176888be55721e9c5ad8
Lee_Klaus <iversonwoool@gmail.com>
2024-03-15 11:46:57 +0800

    编译时，运行时。

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>About</div>
+  <div>About
+  
+    <div>
+      <Profile />
+    </div>
+  </div>
+
 </template>
 
 <script>
+import Vue from 'vue';
+
+// import {} from 'vue'
+
+const Profile = Vue.extend({
+  // functional: true,
+  template: '<div>{{msg}}</div>',
+  data() {
+    return {
+      msg: 'Profile'
+    }
+  },
+  // render(h, ctx) {
+  //   console.log(ctx)
+  //   return (
+  //     <div class="red">{ctx.data.msg}</div>
+  //   )
+  // }
+})
+
 export default {
-  name: 'LkAbout'
+  name: 'LkAbout',
+  components: {
+    Profile,
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.red {
+  background-color: red;
+  color: white;
+}
 </style>
